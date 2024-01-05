06d09daf63b7a483c3c22671bcc8ab8f456db745
SubhradeepSeth <139810175+SubhradeepSeth@users.noreply.github.com>
2024-01-05 11:49:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function shortcut(s1, s2) {
   // your code here
+	if(s1===""||s2===""){
+		return "";
+	}
+	let str=s1.charAt(0)+s2.charAt(0);
+	return str;
+	
 }
 
 // Do not change the code below.
